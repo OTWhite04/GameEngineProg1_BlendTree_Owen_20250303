4116235bc237c5ae07d1aed2ef1e6be26880d7e4
OTWhite04 <owen.t.white2005@gmail.com>
2025-03-05 15:26:54 -0400

    Player controller finished

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,21 +8,28 @@ public class PlayerMovement : MonoBehaviour
    
     //Variable for the move direction.
     private Vector3 moveDirection = Vector3.zero;
-
     //Reference to the animator.
     public Animator animator;
 
-    
+    public float Magnitude;
 
     //Reference to the Rigidbody.
     Rigidbody2D rigidBody;
 
+    //Method for handling the verical and horizontal movement animations for the player.
     private void HandleAnimation()
     {
-        
-
-        animator.SetFloat("Horizontal", moveDirection.x);
-        animator.SetFloat("Vertical", moveDirection.y);
+       
+        if(moveDirection.magnitude != 0)
+        {
+            animator.SetBool("isMoving", true);
+            animator.SetFloat("Horizontal", moveDirection.x);
+            animator.SetFloat("Vertical", moveDirection.y);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
     }
 
     //Float move speed variable.
